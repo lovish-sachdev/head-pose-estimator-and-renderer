52f1af89739661b76378b692a96d33e377f521a8
lovish-sachdev <lovishsachdev454@gmail.com>
2024-06-16 02:27:32 +0530

    try to deploy

--- final_app.py
+++ final_app.py
@@ -11,51 +11,95 @@ import numpy as np
 import time
 import os
 from keras.models import model_from_json
-from streamlit_webrtc import webrtc_streamer,RTCConfiguration,VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer,RTCConfiguration,VideoTransformerBase, WebRtcMode
 import streamlit_webrtc
 import av
+
+
+
+
+from twilio.rest import Client
+
+
+@st.cache_data
+def get_ice_servers():
+    """Use Twilio's TURN server because Streamlit Community Cloud has changed
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
+    """
+
+    # Ref: https://www.twilio.com/docs/stun-turn/api
+    try:
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    except KeyError:
+     
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    token = client.tokens.create()
+
+    return token.ice_servers
 
 
 ## class for transforming and showing image
 class VideoProcessor(VideoTransformerBase):
     def recv(self, frame):
 
-        def logic(image):
-            global img_to_stack,counter,output_img
-            img_h,img_w,img_c=image.shape
-            results=face_mesh.process(image)
-            face_2d=[]
-            if results.multi_face_landmarks:
-                counter+=1
-                counter%=1
-                for face_landmarks in results.multi_face_landmarks:
-                    idx=0
-                    ## indexes of 6 points used during training
-                    for idx in [1,199,263,33,291,61]:
-                        lm= face_landmarks.landmark[idx]
-                        x,y=int(lm.x*img_w),int(lm.y*img_h)
-                        face_2d.append([x,y])
-                        cv2.circle(image,(x,y),2,(0,255,0),thickness=-1)
-                    if counter==0:
-                        face_2d=np.array(face_2d,dtype=np.float64)
-                        data=face_2d.flatten()
-                        data=np.reshape(data,(1,12,))
-                        label=headpose_model.predict(data,verbose=0)[0]
-                        deg_x,deg_y,deg_z,t_x,t_y,t_z=label
-                        img_to_stack=rotate(deg_x,deg_y,deg_z) 
-                        output_img=np.vstack((image,img_to_stack)) 
-                        output_img=cv2.resize(output_img,(640,480))
-                        output_img=output_img.astype("uint8")
-            return output_img
-        frm=frame.to_ndarray(format="bgr24")
-        image=cv2.flip(frm,1)
-        image2=logic(image)
-        return av.VideoFrame.from_ndarray(image2,format="bgr24")
+        # def logic(image):
+        #     global img_to_stack,counter,output_img
+        #     img_h,img_w,img_c=image.shape
+        #     results=face_mesh.process(image)
+        #     face_2d=[]
+        #     if results.multi_face_landmarks:
+        #         counter+=1
+        #         counter%=1
+        #         for face_landmarks in results.multi_face_landmarks:
+        #             idx=0
+        #             ## indexes of 6 points used during training
+        #             for idx in [1,199,263,33,291,61]:
+        #                 lm= face_landmarks.landmark[idx]
+        #                 x,y=int(lm.x*img_w),int(lm.y*img_h)
+        #                 face_2d.append([x,y])
+        #                 cv2.circle(image,(x,y),2,(0,255,0),thickness=-1)
+        #             if counter==0:
+        #                 face_2d=np.array(face_2d,dtype=np.float64)
+        #                 data=face_2d.flatten()
+        #                 data=np.reshape(data,(1,12,))
+        #                 label=headpose_model.predict(data,verbose=0)[0]
+        #                 deg_x,deg_y,deg_z,t_x,t_y,t_z=label
+        #                 img_to_stack=rotate(deg_x,deg_y,deg_z) 
+        #                 output_img=np.vstack((image,img_to_stack)) 
+        #                 output_img=cv2.resize(output_img,(640,480))
+        #                 output_img=output_img.astype("uint8")
+        #     return output_img
+        # try:
+        #     frm=frame.to_ndarray(format="bgr24")
+        #     image=cv2.flip(frm,1)
+        #     image2=logic(image)
+        #     return av.VideoFrame.from_ndarray(image2,format="bgr24")
+        # except:
+        #     return av.VideoFrame.from_ndarray(output_img,format="bgr24")
+        return av.VideoFrame.from_ndarray(frame,format="bgr24")
+
+
 
 def main():
     st.title("Real-time Video Stream using WebRTC")
     vtkObject.GlobalWarningDisplayOff() 
-    webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+    # webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+
+    webrtc_ctx = webrtc_streamer(
+    key="example",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={"iceServers": get_ice_servers()},
+    video_processor_factory=VideoProcessor,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
 
 
 ## function for rotating iron man mask model through specified angles
