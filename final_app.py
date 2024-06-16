d0bbde2cf6f778365935686e641ea4abde27547f
lovish-sachdev <137512011+lovish-sachdev@users.noreply.github.com>
2024-06-16 14:10:25 +0530

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -68,13 +68,13 @@ class VideoProcessor(VideoTransformerBase):
                     if counter==0 and type(face_2d)!=type(None):
                         face_2d=np.array(face_2d,dtype=np.float64)
                         data=face_2d.flatten()
-                        data=np.reshape(data,(1,12,))
-                        label=headpose_model.predict(data,verbose=0)[0]
-                        deg_x,deg_y,deg_z,t_x,t_y,t_z=label
-                        # img_to_stack=rotate(deg_x,deg_y,deg_z) 
-                        output_img=np.vstack((image,img_to_stack)) 
-                        output_img=cv2.resize(output_img,(640,480))
-                        output_img=output_img.astype("uint8")
+                        # data=np.reshape(data,(1,12,))
+                        # label=headpose_model.predict(data,verbose=0)[0]
+                        # deg_x,deg_y,deg_z,t_x,t_y,t_z=label
+                        # # img_to_stack=rotate(deg_x,deg_y,deg_z) 
+                        # output_img=np.vstack((image,img_to_stack)) 
+                        # output_img=cv2.resize(output_img,(640,480))
+                        # output_img=output_img.astype("uint8")
             # return output_img
             return np.ones((480,640,3),dtype="uint8")*200
         frm=frame.to_ndarray(format="bgr24")
